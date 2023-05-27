a4594fdffca0b29590b0b2cb63b131bd1439caf6
Eclipsephage <82249512+Eclipsephage@users.noreply.github.com>
2023-05-27 12:40:11 -0600

    Add files via upload
    must specifiy a model to keep pyg from autoloading

--- webui_setup.sh
+++ webui_setup.sh
@@ -53,15 +53,16 @@ cd /text-generation-webui
 echo -e "\e[32mInitializing server...\e[0m"
 
 # Run server and get its PID
-if [ "$SILLY_TAVERN" = "y" ]
+#Note: setting the model keeps ooba from autoloading pyg
+if [ "$SILLY_TAVERN" = "y"   
 then
-	python server.py --share --public-api --api --trust-remote-code --chat --auto-devices --model_type llama --extension whisper_stt api &
+	python server.py --share --public-api --api --trust-remote-code --chat --auto-devices --model llama --model_type llama --extension whisper_stt api &
 
 else
-    python server.py --share --public-api --api --trust-remote-code --chat --auto-devices --model_type llama --extension whisper_stt elevenlabs_tts api &
+    python server.py --share --public-api --api --trust-remote-code --chat --auto-devices --model llama --model_type llama --extension whisper_stt elevenlabs_tts api &
 fi
 
-sleep 15
+sleep 12
 SERVER_PID=$!
 echo -e "\e[32mServer PID is $SERVER_PID\e[0m"
 echo -e "\e[32mStarting server... press Ctrl-C and re-run server.py if fails\e[0m"
@@ -71,9 +72,9 @@ sleep 5
 if [ "$SILLY_TAVERN" = "y" ]
 then
 	echo -e "\e[32mrunning python server.py --share --public-api --api --trust-remote-code --chat --auto-devices --model_type llama --extension whisper_stt api\e[0m"
-	python server.py --share --public-api --api --trust-remote-code --chat --auto-devices --model_type llama --extension whisper_stt api
+	python server.py --share --public-api --api --trust-remote-code --chat --auto-devices --model llama --model_type llama --extension whisper_stt api
 
 else
 	echo -e "\e[32mrunning python server.py --share --public-api --api --trust-remote-code --chat --auto-devices --model_type llama --extension whisper_stt elevenlabs_tts api\e[0m"
-    python server.py --share --public-api --api --trust-remote-code --chat --auto-devices --model_type llama --extension whisper_stt elevenlabs_tts api
+    python server.py --share --public-api --api --trust-remote-code --chat --auto-devices model llama --model_type llama --extension whisper_stt elevenlabs_tts api
 fi
